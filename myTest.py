a2602eeb8f8ca458c1e419ec42d54f87bdfb25c8
cudmore <robert.cudmore@gmail.com>
2021-11-04 10:02:47 -0700

    third

---
+++ myTest.py
@@ -0,0 +1,199 @@
+"""
+Working out to write code to interact with Napari viewer.
+
+In particular
+    - USing either a Shape or Points layer
+    - be notified of new shapes/points
+    - be notified of user changing a point/shape (by dragging)
+    - be notified when a shape/point is deleted
+"""
+
+import time
+import napari
+from napari.qt import thread_worker
+from skimage.io import imread
+from skimage.morphology import disk, ball
+from skimage.filters.rank import gradient
+
+import numpy as np
+import pandas as pd
+
+def _loadLine(path):
+    skiprows = 7  # todo: read this from header comment
+    df = pd.read_csv(path, skiprows=skiprows)
+    print(df.head())
+
+path = '/home/cudmore/Downloads/bil/d9/01/rr30a_s1_ch2.tif'
+linePath = '/home/cudmore/Downloads/bil/d9/01/line/rr30a_s0_l.txt'
+
+_loadLine(linePath)
+
+image = imread(path)
+#print(image.shape)
+
+#image = image[20,:,:]
+#image = image.astype(np.uint8)
+
+# 2
+viewer = napari.Viewer()
+
+imageLayer = viewer.add_image(image, colormap='green', blending='additive')
+
+points = np.array([[100, 100], [200, 200], [300, 100]])
+pointsLayer = viewer.add_points(points, size=30)
+
+#viewer.add_image(gradient(image, disk(5)), name='gradient', colormap='magenta', blending='additive')
+
+#viewer.add_shapes([[ 100,80], [140, 150]], shape_type='path', edge_color='cyan', edge_width=3)
+
+# 3
+#from napari_plot_profile import PlotProfile
+#profiler = PlotProfile(viewer)
+#viewer.window.add_dock_widget(profiler, area='right')
+
+# 4
+#profiler._list_values()
+
+def mySelectionChanged(event):
+    print('=== mySelectionChanged()')
+    
+def print_layer_name(event):
+    """
+    event is <class 'napari.utils.events.event.Event'>
+    
+    event.source.selected_data is a set of selected point indices
+    
+    use just the selected points to update out table
+
+    on highlight, use selected_data {} and compare to stored value
+    to determine if it is new
+    """
+    print('=== print_layer_name()')
+    
+    #print('vars(event):', vars(event))
+    
+    print('  type(event)', type(event))
+    
+    # event.source is the layer (like Points or Shapes)
+    print('  type(event.source)', type(event.source))
+    
+    # event.source is a HUGE object
+    #print('vars(event.source):', vars(event.source))
+    #print('   ', type(event.source))
+    
+    # event.type is in {'interactive', 'mode', 'data'}
+    print('  type(event.type):', type(event.type))
+    print('  event.type:', event.type)
+    
+    # name is name of layer(can be set by user in interface)
+    print('  event.source.name:', event.source.name)
+    
+    # mode is {'add', 'select', 'pan-zoom'}
+    print('  event.source.mode:', event.source.mode)
+
+    # 1) when mode is 'add' we get added point index in selected_data set
+    # point 3 would be selected_data={3}
+    # 2) when mode is 'select' and user finishes dragging we get point that was moved
+    # 3) when user deletes a point, select_data=set()
+    #    we need to keep track of selected point with worker 'selectedDataWorker'(below)
+    #    When we delete, be sure to set local copy of selectedData to none (handled in worker???)
+    print('  event.source.selected_data:', event.source.selected_data)
+
+    # list of list of points
+    try:
+        print('  event.value:', event.value)
+    except (AttributeError) as e:
+        print('  value: AttributeError')
+
+    # event.source.data is a list of list of points (can be 1000's)
+    #print('  data:', event.source.data)
+    
+    # see: https://napari.org/docs/0.3.7/_modules/napari/layers/points/points.html
+    # we can always query layer for state (we can also set)
+    # print('  pointsLayer.mode:', pointsLayer.mode)
+
+
+# when we switch between layers
+viewer.layers.selection.events.changed.connect(mySelectionChanged)
+
+pointsLayer.events.data.connect(print_layer_name)
+# these work
+# for a list of events, see
+# https://napari.org/docs/0.3.7/_modules/napari/layers/base/base.html
+#pointsLayer.events.refresh.connect(print_layer_name)
+#pointsLayer.events.set_data.connect(print_layer_name)
+#pointsLayer.events.mode.connect(print_layer_name)
+#pointsLayer.events.interactive.connect(print_layer_name)
+#pointsLayer.events.status.connect(print_layer_name)
+
+# select/deselect are depreciated and do nothing
+# I think they were to signal when entire layer is selected (no a single point/shape)
+#pointsLayer.events.select.connect(print_layer_name)
+#pointsLayer.events.deselect.connect(print_layer_name)
+
+'''
+def xxxOnSelection(event):
+    print('xxx')
+viewer.layers.selection.events.changed.connect(xxxOnSelection)
+'''
+
+# no work
+# pointsLayer.events.inserting.connect(print_layer_name)
+
+# viewer.layers.selection.events.changed
+# inspect added and removed
+#pointsLayer.selection.events.changed(print_layer_name)
+
+# this triggers on mouse move
+#pointsLayer.events.highlight.connect(print_layer_name)
+
+# no work
+# pointsLayer.events.add.connect(print_layer_name)
+
+# I need this to get selected point (why so complicated)
+# This simply polls pointsLayer.selected_data and trigger when it changes
+def selectedDataWorker(pointsLayer, poll = 1):  
+    """
+    Show selected data
+    """
+    def onSelectedDataChanged(selectedData):
+        if selectedData is not None:
+            #if len(selectedData) > 0:
+            
+            # triggered when mode='select' and user click a point/shape
+            # When user clicks canvas, all points are de-selected
+            #   selectedData is empty set()
+            print()
+            print('  === onSelectedDataChanged() WORKER', selectedData)
+            print()
+
+    """
+    Listen to selected data changes
+    When connect parameter is not None the worker starts automatically
+      Otherwise need to call worker.start()
+    """
+    @thread_worker(connect={'yielded': onSelectedDataChanged})
+    def _watchSelectedData(pointsLayer):
+        selectedData = None
+
+        while True:
+            time.sleep(poll)
+            oldSelectedData = selectedData
+            
+            # here we are repeatedly polling the pointsLayer.selected_data
+            selectedData = pointsLayer.selected_data
+
+            if oldSelectedData != selectedData:
+                yield selectedData
+
+            yield None
+
+    return(_watchSelectedData(pointsLayer))
+
+worker = selectedDataWorker(
+    pointsLayer,
+    poll = 1/10
+)
+
+#
+napari.run() 
---
+++ sandbox/myMagicGui.py
@@ -0,0 +1,149 @@
+"""
+Working out how to create magicgui interface objects
+
+    - All magicgui interface objects have underlying Qt objects
+        For example, a mgicgui 'Table' has and underlying QTableWidget object
+"""
+import pandas as pd
+
+from magicgui import magicgui
+from magicgui.widgets import Table
+
+from enum import Enum
+
+#
+# Example off magicgui website
+# 
+class Medium(Enum):
+    Glass = 1.520
+    Oil = 1.515
+    Water = 1.333
+    Air = 1.0003
+
+# decorate your function with the @magicgui decorator
+@magicgui(call_button="calculate", result_widget=True, layout='vertical')
+def snells_law(aoi=30.0, n1=Medium.Glass, n2=Medium.Water, degrees=True):
+    import math
+
+    aoi = math.radians(aoi) if degrees else aoi
+    try:
+        result = math.asin(n1.value * math.sin(aoi) / n2.value)
+        return math.degrees(result) if degrees else result
+    except ValueError:
+        return "Total internal reflection!"
+
+# your function is now capable of showing a GUI
+
+
+#
+# Working out how to use magicgui Table (e.g. a QTableWidget)
+
+# load some data
+df = pd.read_csv('data/iris.csv')
+print(len(df))
+print(df.head())
+dfDict = df.to_dict()
+
+def myTableChanged(d : dict):
+    """
+    Respond to user editing a single cell.
+
+    This is a convenience wrapper/callback provided by magicgui?
+
+    
+    Params:
+        d (dict): Dictionary of new value and underlying row and column
+    
+        For example:
+            d = {'data': <value>, 'row': int, 'column': int, 'column_header': str, 'row_header': str}
+    
+            Where <value> of 'data' will be new 'value'
+            and <value> will be proper type, str if str, int if int, float if float
+            does not support list or dict (they are received as str)
+    """
+    print('=== (1) myTableChanged()')
+    print(f"  d['data'] is "{d['data']}"" with type: {type(d['data'])}")
+    print(d)
+
+# see: https://doc.qt.io/qt-5.15/qtablewidget.html
+# for definitions of (itemChanged, itemClicked)
+def onItemChanged(item):
+    """
+    Resond to user editing a single cell.
+    
+    This is a native callback for PyQt?
+
+    Params:
+        item (QTableWidgetItem): The item that was edited
+
+    Note:
+        item.text() is the new str representation of the value (it is always a str)
+    """
+    print('=== (2) onItemChanged()')
+    print('  item.row:', item.row())
+    print('  item.col:', item.column())
+    newText = item.text()
+    print(f'  item.text: "{newText}" type:{type(newText)}')
+    # ...
+    # whatever you want to do with the new value
+
+def onItemClicked(item):
+    """
+    Respond to user clicking table.
+    
+    Params:
+        item (QTableWidgetItem): The item clicked
+    """
+    print('=== onItemClicked item:', item)
+    print('  row:', item.row())
+    print('  col:', item.column())
+
+# Create a magicgui table from a dict
+myTable = Table(value=dfDict)
+
+# this seems to be provided as a convenience function from napari/magicgui
+myTable.changed.connect(myTableChanged)
+
+# this returns <magicgui.backends._qtpy.widgets._QTableExtended object at 0x7f4871304c10>
+# print('myTable.native:', myTable.native)
+
+# returns <magicgui.backends._qtpy.widgets.Table object at 0x7f258a5cfbe0>
+#print(myTable._widget)
+
+
+myTable.native.itemChanged.connect(onItemChanged)
+
+myTable.native.itemClicked.connect(onItemClicked)
+
+# Working on getting the underlying QTableWidget from the magicgui Table
+
+# my style
+from PyQt5 import QtWidgets
+
+# their style
+#from qtpy import QtWidgets as QtW
+
+# both of these report type <class 'magicgui.backends._qtpy.widgets._QTableExtended'>
+#   but ... it seems to really be a QTableWidget
+print('myTable._widget._qwidget is:', type(myTable._widget._qwidget))
+print('myTable.native is:', type(myTable.native))
+
+# set selection modes so we can only select one row
+myTable.native.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
+myTable.native.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+
+# works
+#myTable._widget._qwidget.setSelectionMode(QtW.QAbstractItemView.SingleSelection)
+#myTable._widget._qwidget.setSelectionBehavior(QtW.QAbstractItemView.SelectRows)
+
+# programatically select a row
+# myTable._widget._qwidget.selectRow(3)
+myTable.native.selectRow(3)
+
+
+if __name__ == "__main__":
+    # this works
+    # snells_law.show(run=False)
+    
+    # this works
+    myTable.show(run=True)
