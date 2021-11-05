e6e14e4d64ebb1dddacaa3f069caf100515442ed
cudmore <robertcudmore@gmail.com>
2021-11-05 13:45:20 -0700

    from work

--- myTest2.py
+++ myTest2.py
@@ -2,11 +2,12 @@
 """
 
 import time
+import tifffile
 import napari
 from napari.qt import thread_worker
-from skimage.io import imread
-from skimage.morphology import disk, ball
-from skimage.filters.rank import gradient
+#from skimage.io import imread
+#from skimage.morphology import disk, ball
+#from skimage.filters.rank import gradient
 
 import numpy as np
 import pandas as pd
@@ -31,19 +32,19 @@ class myClass():
 
         self._worker = self.selectedDataWorker(pointsLayer, poll = 1/10)
 
-    def selectedDataWorker(self, pointsLayer, poll = 1):  
+    def selectedDataWorker(self, pointsLayer, poll = 1):
         """
         Show selected data
         """
         def onSelectedDataChanged(selectedData):
             if selectedData is not None:
                 #if len(selectedData) > 0:
-                
+
                 # triggered when mode='select' and user click a point/shape
                 # When user clicks canvas, all points are de-selected
                 #   selectedData is empty set()
                 self._mySelectedPoint = self._getPointFromSet(selectedData)
-                
+
                 print()
                 print('  === onSelectedDataChanged() WORKER', selectedData)
                 print(f'    selectedData: {selectedData}')
@@ -62,7 +63,7 @@ class myClass():
             while True:
                 time.sleep(poll)
                 oldSelectedData = selectedData
-                
+
                 # here we are repeatedly polling the pointsLayer.selected_data
                 selectedData = pointsLayer.selected_data
 
@@ -80,7 +81,7 @@ class myClass():
         print('=== print_layer_name()')
 
         selectedDataIsEmpty = event.source.selected_data == set()
-        
+
         isAdd = event.source.mode == 'add'
         isFinishedDrag = event.source.mode == 'select' and not selectedDataIsEmpty
         isDelete = event.source.selected_data == set()
@@ -108,10 +109,10 @@ class myClass():
     def _getPointFromSet(self, setOfPnts):
         """
         Retun one index from a set of index.
-        
+
         Args:
             setOfPnts (set): Set of point indices
-        
+
         Note:
             todo: currently modifies setOfPnts with pop !!!
         """
@@ -123,7 +124,7 @@ class myClass():
 
 if __name__ == '__main__':
     path = '/home/cudmore/Downloads/bil/d9/01/rr30a_s1_ch2.tif'
-    image = imread(path)
+    image = tifffile.imread(path)
 
     viewer = napari.Viewer()
 
@@ -134,4 +135,4 @@ if __name__ == '__main__':
 
     aMyClass = myClass(viewer,pointsLayer)
     #
-    napari.run() 
+    napari.run()
